83f5d55531aeca03a44033893ee31587672c566d
lee <l_joose@163.com>
2019-06-03 11:43:17 +0800

    dev branch commit test

--- src/main/java/com/crazy/portal/PortalApplication.java
+++ src/main/java/com/crazy/portal/PortalApplication.java
@@ -25,6 +25,7 @@ public class PortalApplication extends SpringBootServletInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		super.onStartup(servletContext);
 
+
 		//start-up system
 	}
 
