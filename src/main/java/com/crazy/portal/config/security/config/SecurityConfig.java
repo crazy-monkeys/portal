d2120331a6b0196e5afd759b54ad38f0d7472006
bill <biao.chenit@qq.com>
2019-06-13 21:28:29 +0800

    静态资源  忽略权限

--- src/main/java/com/crazy/portal/config/security/config/SecurityConfig.java
+++ src/main/java/com/crazy/portal/config/security/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/static/**","/view/**","/login","/").permitAll()
+                .antMatchers("/login","/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().disable()
