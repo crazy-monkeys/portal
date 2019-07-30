af10b3bd389ab7f0948d55f27c324457012bf8c4
junguohe <wy.950202>
2019-07-30 21:22:56 +0800

    Test

---
+++ src/main/java/com/crazy/portal/bean/order/OrderVO.java
@@ -0,0 +1,13 @@
+package com.crazy.portal.bean.order;
+
+import lombok.Data;
+
+/**
+ * Created by weiying on 2019/7/29.
+ */
+@Data
+public class OrderVO {
+    private String orderNo;
+    private String orderStatus;
+    private String approvalRemark;
+}
