eaa85626893382a9c3c36360e33d6dea765d3d42
junguohe <wy.950202>
2019-07-30 13:59:06 +0800

    Test

---
+++ src/main/java/com/crazy/portal/controller/order/OrderController.java
@@ -0,0 +1,28 @@
+package com.crazy.portal.controller.order;
+
+import com.alibaba.fastjson.JSON;
+import com.crazy.portal.bean.BaseResponse;
+import com.crazy.portal.bean.order.OrderVO;
+import com.crazy.portal.controller.BaseController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by weiying on 2019/7/29.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/order")
+public class OrderController extends BaseController{
+
+    /**
+     * ECC 订单状态更新test api
+     * @param orderVO
+     * @return
+     */
+    @PostMapping("/status/update")
+    public BaseResponse updateOrderStatus(@RequestBody OrderVO orderVO){
+        log.info("order param"+ JSON.toJSONString(orderVO));
+        return successResult();
+    }
+}
