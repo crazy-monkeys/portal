5e4fc950be4b78550d19bbf704cc9554f36f3a27
biao.chenit@qq.com <bill572004>
2019-07-30 22:11:46 +0800

    删除demo使用的页面配置

--- src/main/java/com/crazy/portal/config/WebConfig.java
+++ src/main/java/com/crazy/portal/config/WebConfig.java
@@ -64,14 +64,6 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
-    }
-
-    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/file/**").addResourceLocations("file:".concat(filepath));
     }
