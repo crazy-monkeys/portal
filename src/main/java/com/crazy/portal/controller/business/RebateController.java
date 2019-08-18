3b7a2c4a36a7d19ff9dbab57093731fe19acd3bf
xiaxingit <947516986@qq.com>
2019-08-18 12:05:38 +0800

    modify 客户Rebate

--- src/main/java/com/crazy/portal/controller/business/RebateController.java
+++ src/main/java/com/crazy/portal/controller/business/RebateController.java
@@ -3,6 +3,7 @@ package com.crazy.portal.controller.business;
 import com.crazy.portal.bean.BaseResponse;
 import com.crazy.portal.bean.business.rebate.RebateQueryBean;
 import com.crazy.portal.controller.BaseController;
+import com.crazy.portal.entity.business.rebate.BusinessRebate;
 import com.crazy.portal.service.business.RebateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -26,26 +27,27 @@ public class RebateController extends BaseController {
 
     @GetMapping("/list")
     public BaseResponse list(RebateQueryBean bean){
-        return successResult();
+        return successResult(rebateService.list(bean));
     }
 
     @GetMapping("/find/{id}")
     public BaseResponse find(@PathVariable Integer id){
-        return successResult();
+        return successResult(rebateService.find(id));
     }
 
     @PostMapping("/confirm")
-    public BaseResponse confirm(){
+    public BaseResponse confirm(@RequestBody BusinessRebate bean){
+        rebateService.confirm(bean, getCurrentUserId());
         return successResult();
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/upload/{id}")
     public BaseResponse upload(@PathVariable Integer id, MultipartFile file){
-        return successResult();
+        return successResult(rebateService.fileUpload(id, getCurrentUserId(), file));
     }
 
     @GetMapping("/download/{id}")
     public void download(@PathVariable Integer id, HttpServletResponse response){
-
+        rebateService.fileDownload(id, response);
     }
 }
--- src/main/java/com/crazy/portal/service/business/RebateService.java
+++ src/main/java/com/crazy/portal/service/business/RebateService.java
@@ -1,17 +1,29 @@
 package com.crazy.portal.service.business;
 
 import com.crazy.portal.bean.business.rebate.RebateQueryBean;
+import com.crazy.portal.bean.common.Constant;
+import com.crazy.portal.bean.customer.basic.FileVO;
 import com.crazy.portal.dao.business.rebate.BusinessAccountDetailMapper;
 import com.crazy.portal.dao.business.rebate.BusinessRebateFileMapper;
 import com.crazy.portal.dao.business.rebate.BusinessRebateMapper;
 import com.crazy.portal.dao.business.rebate.BusinessStrategyMapper;
+import com.crazy.portal.entity.basic.BasicFile;
 import com.crazy.portal.entity.business.rebate.BusinessRebate;
+import com.crazy.portal.entity.business.rebate.BusinessRebateFile;
+import com.crazy.portal.util.DateUtil;
+import com.crazy.portal.util.Enums;
+import com.crazy.portal.util.FileUtil;
 import com.crazy.portal.util.PortalUtil;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -31,18 +43,81 @@ public class RebateService {
     private BusinessAccountDetailMapper businessAccountDetailMapper;
     @Resource
     private BusinessStrategyMapper businessStrategyMapper;
+    @Value("${file.path.root}")
+    private String filePath;
+    private static final String CUST_FILE_PATH = "rebate";
 
+    /**
+     * 客户rebate列表
+     * @param bean
+     * @return
+     */
     public PageInfo<BusinessRebate> list(RebateQueryBean bean){
         PortalUtil.defaultStartPage(bean.getPageIndex(), bean.getPageSize());
         List<BusinessRebate> list = businessRebateMapper.selectByPage(bean);
         return new PageInfo<>(list);
     }
 
+    /**
+     * 客户rebate明细查询
+     * @param id
+     * @return
+     */
     public BusinessRebate find(Integer id){
         BusinessRebate info = businessRebateMapper.selectByPrimaryKey(id);
         info.setFile(businessRebateFileMapper.selectByRebateId(id));
         info.setAccountDetailList(businessAccountDetailMapper.selectByRebateId(id));
         info.setStrategyList(businessStrategyMapper.selectByRebateId(id));
         return info;
+    }
+
+    /**
+     * 发送确认函
+     * @param bean
+     * @param userId
+     */
+    public void confirm(BusinessRebate bean, Integer userId){
+        bean.setUpdateId(userId);
+        bean.setUpdateTime(DateUtil.getCurrentTS());
+        bean.setStatus(Enums.BusinessRebateStatus.CS_CONFIRM.getCode());
+        businessRebateMapper.updateByPrimaryKeySelective(bean);
+    }
+
+    /**
+     * 文件上传
+     * @param files
+     * @return
+     */
+    public FileVO fileUpload(Integer id, Integer userId, MultipartFile files){
+        FileVO fileInfo = FileUtil.upload(files, getCustFilePath());
+        BusinessRebate record = businessRebateMapper.selectByPrimaryKey(id);
+        record.setStatus(Enums.BusinessRebateStatus.FINISHED.getCode());
+        record.setUpdateId(userId);
+        record.setUpdateTime(DateUtil.getCurrentTS());
+        businessRebateMapper.updateByPrimaryKeySelective(record);
+
+        BusinessRebateFile rebateFile = new BusinessRebateFile();
+        rebateFile.setRebateId(id);
+        rebateFile.setFileName(fileInfo.getFileName());
+        rebateFile.setFilePath(fileInfo.getFullPath());
+        rebateFile.setActive(Constant.ACTIVE);
+        rebateFile.setCreateId(userId);
+        rebateFile.setCreateTime(DateUtil.getCurrentTS());
+        businessRebateFileMapper.insertSelective(rebateFile);
+
+        return fileInfo;
+    }
+
+    /**
+     * 文件下载
+     * @param response
+     */
+    public void fileDownload(Integer id, HttpServletResponse response){
+        BusinessRebateFile file = businessRebateFileMapper.selectByRebateId(id);
+        FileUtil.download(response, getCustFilePath(), file.getFileName());
+    }
+
+    public String getCustFilePath(){
+        return filePath.concat(File.separator).concat(CUST_FILE_PATH).concat(File.separator);
     }
 }
