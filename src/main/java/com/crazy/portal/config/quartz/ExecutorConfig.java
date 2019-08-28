bff3bf5ea10acb0dd60b769424fd1b4cf3be6f32
biao.chen <biao.chen@baozun.com>
2019-08-28 23:00:53 +0800

    异步线程加入线程池

--- src/main/java/com/crazy/portal/config/quartz/ExecutorConfig.java
+++ src/main/java/com/crazy/portal/config/quartz/ExecutorConfig.java
@@ -2,24 +2,33 @@ package com.crazy.portal.config.quartz;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import javax.annotation.Resource;
-import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
+@EnableAsync
 public class ExecutorConfig {
 
-    @Resource
-    private JobFactory jobFactory;
+    /** Set the ThreadPoolExecutor's core pool size. */
+    private int corePoolSize = 15;
+    /** Set the ThreadPoolExecutor's maximum pool size. */
+    private int maxPoolSize = 50;
+    /** Set the capacity for the ThreadPoolExecutor's BlockingQueue. */
+    private int queueCapacity = 15;
+
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(){
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-        factory.setJobFactory(jobFactory);
-        Properties properties = new Properties();
-        properties.setProperty("org.quartz.scheduler.skipUpdateCheck","true");
-        factory.setQuartzProperties(properties);
-        return factory;
+    public Executor myAsync() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix("Task-Executor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
     }
 }
