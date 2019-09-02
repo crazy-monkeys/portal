edd2ca28e2a5b03866163c396d6905eb67543b25
biao.chen <biao.chen@baozun.com>
2019-09-02 23:12:03 +0800

    代码重构

--- src/main/java/com/crazy/portal/filter/AuthFilter.java
+++ src/main/java/com/crazy/portal/filter/AuthFilter.java
@@ -5,6 +5,9 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 权限过滤器
@@ -13,7 +16,7 @@ import java.io.IOException;
  */
 public class AuthFilter implements Filter{
 
-	String[] includeUrls = new String[]{"/login", "/static"};
+	private final static String[] includeUrls = new String[]{"/login", "/static"};
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,FilterChain filterChain)
@@ -35,11 +38,10 @@ public class AuthFilter implements Filter{
 	 * @Description: 是否需要过滤
 	 */
 	public boolean isNeedFilter(String uri) {
-		for (String includeUrl : includeUrls) {
-			if(includeUrl.equals(uri) || uri.contains(includeUrl)) {
-				return false;
-			}
-		}
-		return true;
+
+		List<String> list = Stream.of(includeUrls)
+				.filter(x->x.equals(uri) || uri.contains(x)).collect(Collectors.toList());
+
+		return list.isEmpty();
 	}
 }
