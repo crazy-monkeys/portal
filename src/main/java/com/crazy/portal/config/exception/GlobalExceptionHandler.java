d8350d823275a8014d9e8a2350d436f259e9a07f
biao.chen <biao.chen@baozun.com>
2019-09-02 22:55:52 +0800

    代码重构

--- src/main/java/com/crazy/portal/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/crazy/portal/config/exception/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.crazy.portal.config.exception;
 
 import com.crazy.portal.bean.BaseResponse;
+import com.crazy.portal.util.ErrorCodes.CommonEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,26 +22,26 @@ public class GlobalExceptionHandler {
     @ResponseBody
     @ExceptionHandler(value = Exception.class)
     public BaseResponse parameterExceptionHandler(Exception exception) {
-
         log.error("", exception);
-        BaseResponse response = new BaseResponse();
+
         if(exception instanceof MethodArgumentNotValidException){
+
             MethodArgumentNotValidException paramException = (MethodArgumentNotValidException) exception;
             List<ObjectError> errorList = paramException.getBindingResult().getAllErrors();
-            String msg = errorList.stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(","));
-            response.fail(-1, msg);
-            return response;
+
+            String msg = errorList.stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .collect(Collectors.joining(","));
+
+            return new BaseResponse(CommonEnum.REQ_PARAM_FORMAT_ERROR.getCode(),msg);
         }
 
         if(exception instanceof BusinessException){
             BusinessException ex = (BusinessException)exception;
-            response.setCode(ex.getErrorCode());
-            response.setMsg(ex.getMessage());
-            return response;
+            return new BaseResponse(ex.getErrorCode(),ex.getMessage());
         }
 
-        response.systemException();
-        return response;
+        return new BaseResponse(CommonEnum.SYSTEM_EXCEPTION.getCode(),CommonEnum.SYSTEM_EXCEPTION.getZhMsg());
     }
 
 }
