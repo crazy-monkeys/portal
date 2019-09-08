6646f3e67cee4bd01745254370637a29f55e99b0
junguohe <wy!950202>
2019-09-08 11:55:33 +0800

    test

--- src/test/java/com/crazy/portal/PortalApplicationTests.java
+++ src/test/java/com/crazy/portal/PortalApplicationTests.java
@@ -1,17 +1,33 @@
 package com.crazy.portal;
 
+import com.crazy.portal.config.email.EmailHelper;
+import com.crazy.portal.dao.system.TemAddressMapper;
+import com.crazy.portal.util.CallApiUtils;
+import com.crazy.portal.util.FTPClientUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PortalApplicationTests {
 
+	@Resource
+	private EmailHelper emailHelper;
+
+	@Resource
+	private TemAddressMapper temAddressMapper;
+
+
+	@Resource
+	private FTPClientUtil ftpClientUtil;
 
 	@Test
 	public void createUser(){
-
+		CallApiUtils.queryOrganisation();
+		//CallApiUtils.callECCCreditApi("10217");
 	}
 }
