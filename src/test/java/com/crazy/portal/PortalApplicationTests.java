8ac1dc446efbd87e35ef563c0bef7141b6699308
junguohe <wy!950202>
2019-09-11 11:51:05 +0800

    拜访记录

--- src/test/java/com/crazy/portal/PortalApplicationTests.java
+++ src/test/java/com/crazy/portal/PortalApplicationTests.java
@@ -1,6 +1,15 @@
 package com.crazy.portal;
 
+import com.alibaba.fastjson.JSON;
+import com.crazy.portal.bean.customer.wsdl.orgnation.OrganisationalUnitByElementsResponseSync;
+import com.crazy.portal.bean.customer.wsdl.orgnation.OrganisationalUnitByIDResponseMessageSync;
+import com.crazy.portal.bean.customer.wsdl.orgnation.OrganisationalUnitQueryRepsonseNameAndAddress;
+import com.crazy.portal.bean.customer.wsdl.orgnation.OrganisationalUnitQueryResponseEmployeeAssignment;
 import com.crazy.portal.config.email.EmailHelper;
+import com.crazy.portal.dao.system.InternalUserMapper;
+import com.crazy.portal.dao.system.OrganizationalStructureMapper;
+import com.crazy.portal.entity.system.InternalUser;
+import com.crazy.portal.entity.system.OrganizationalStructure;
 import com.crazy.portal.util.CallApiUtils;
 import com.crazy.portal.util.FTPClientUtil;
 import org.junit.Test;
@@ -9,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,9 +30,57 @@ public class PortalApplicationTests {
 	@Resource
 	private FTPClientUtil ftpClientUtil;
 
+	@Resource
+	private OrganizationalStructureMapper organizationalStructureMapper;
+	@Resource
+	private InternalUserMapper internalUserMapper;
+
 	@Test
 	public void createUser(){
-		CallApiUtils.queryOrganisation();
+		//System.out.println(JSON.toJSONString(CallApiUtils.queryOrganisation()));
 		//CallApiUtils.callECCCreditApi("10217");
+	}
+
+	@Test
+	public void test(){
+		/*try{
+			OrganisationalUnitByIDResponseMessageSync responseMessageSync = CallApiUtils.queryOrganisation();
+			List<OrganisationalUnitByElementsResponseSync> results = responseMessageSync.getOrganisationalUnit();
+			for(OrganisationalUnitByElementsResponseSync org : results){
+				OrganizationalStructure organizational = organizationalStructureMapper.selectByOrgNo(org.getID());
+				if(null == organizational){
+					organizational = new OrganizationalStructure();
+				}
+				organizational.setActive(1);
+				organizational.setSeq(Integer.valueOf(org.getID()));
+				if(null != org.getValidityPeriod()){
+					String startDate = org.getValidityPeriod().getStartDate().getYear()+"-"+org.getValidityPeriod().getStartDate().getMonth()+"-"+org.getValidityPeriod().getStartDate().getDay();
+					String endDate = org.getValidityPeriod().getEndDate().getYear()+"-"+org.getValidityPeriod().getEndDate().getMonth()+"-"+org.getValidityPeriod().getEndDate().getDay();
+					organizational.setValidTime(startDate);
+					organizational.setInvalidTime(endDate);
+				}
+				List<OrganisationalUnitQueryRepsonseNameAndAddress> names = org.getNameAndAddress();
+				for(OrganisationalUnitQueryRepsonseNameAndAddress name : names){
+					organizational.setOrgName(name.getName());
+				}
+				List<OrganisationalUnitQueryResponseEmployeeAssignment> emps = org.getEmployeeAssignment();
+				for(OrganisationalUnitQueryResponseEmployeeAssignment emp : emps){
+					if(null != emp.getRoleCode()){
+						organizational.setParentOrg(Integer.valueOf(emp.getRoleCode()));
+					}
+					if(null != emp.getEmployeeInternalID()){
+						InternalUser internalUser = internalUserMapper.selectByUserNo(emp.getEmployeeInternalID());
+						organizational.setPm(internalUser==null?"":internalUser.getUserName());
+					}
+				}
+				if(null != organizational.getId()){
+					organizationalStructureMapper.updateByPrimaryKeySelective(organizational);
+				}else{
+					organizationalStructureMapper.insertSelective(organizational);
+				}
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+		}*/
 	}
 }
