4a94051320400579e0dd96b5b4015b04f0a8c61e
biao.chen <biao.chen@baozun.com>
2019-09-18 00:26:47 +0800

    添加注释

--- src/test/java/com/crazy/portal/service/price/CatalogPriceServiceTest.java
+++ src/test/java/com/crazy/portal/service/price/CatalogPriceServiceTest.java
@@ -1,6 +1,5 @@
 package com.crazy.portal.service.price;
 
-import com.alibaba.fastjson.JSON;
 import com.crazy.portal.bean.price.BICatalogPrice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
@@ -8,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import javax.annotation.Resource;
 import java.util.List;
 
