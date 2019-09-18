11cec380297b6639f4217b80b7c00c2c72c07ac0
biao.chen <biao.chen@baozun.com>
2019-09-18 23:36:06 +0800

    新加查询条件

--- src/main/java/com/crazy/portal/bean/price/CatalogPriceVO.java
+++ src/main/java/com/crazy/portal/bean/price/CatalogPriceVO.java
@@ -22,6 +22,8 @@ public class CatalogPriceVO extends PageBean {
 
     private String productModel;
 
+    private String productType;
+
     private String status;
 
     private String inCustomer;
