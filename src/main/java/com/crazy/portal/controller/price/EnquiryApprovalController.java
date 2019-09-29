1082dff0ae69f168e93bbab3749f7cce67b5b4ab
biao.chen <biao.chen@baozun.com>
2019-09-29 22:09:52 +0800

    用户管理-冻结

--- src/main/java/com/crazy/portal/controller/price/EnquiryApprovalController.java
+++ src/main/java/com/crazy/portal/controller/price/EnquiryApprovalController.java
@@ -1,19 +1,19 @@
 package com.crazy.portal.controller.price;
 
-import com.alibaba.fastjson.JSON;
 import com.crazy.portal.bean.BaseResponse;
 import com.crazy.portal.bean.price.EnquiryApprovalBean;
 import com.crazy.portal.bean.price.EnquiryPriceVO;
 import com.crazy.portal.controller.BaseController;
 import com.crazy.portal.entity.price.EnquiryPrice;
 import com.crazy.portal.service.price.EnquiryPriceService;
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @Desc: 询价审批
