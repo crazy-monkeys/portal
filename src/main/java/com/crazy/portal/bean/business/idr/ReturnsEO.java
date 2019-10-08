190352d9a602a00683292cebbd3c22f6c4a0eff7
xiaxingit <xinxia123#>
2019-10-08 23:59:45 +0800

    modify 退换货模板

--- src/main/java/com/crazy/portal/bean/business/idr/ReturnsEO.java
+++ src/main/java/com/crazy/portal/bean/business/idr/ReturnsEO.java
@@ -12,33 +12,59 @@ import java.math.BigDecimal;
 @Data
 public class ReturnsEO extends BaseRowModel {
 
-    @ExcelProperty(value = "类型", index = 0)
-    private String type;
-    @ExcelProperty(value = "订单号", index = 1)
-    private String orderNumber;
+    @ExcelProperty(value = "发货公司", index = 0)
+    private String shipmentCompany;
+    @ExcelProperty(value = "代理商", index = 1)
+    private String agencyName;
     @ExcelProperty(value = "提货日期", index = 2)
     private String takeGoodsDate;
-    @ExcelProperty(value = "产品线", index = 3)
-    private String productLine;
-    @ExcelProperty(value = "BU", index = 4)
-    private String bu;
-    @ExcelProperty(value = "PDT", index = 5)
-    private String pdt;
-    @ExcelProperty(value = "平台", index = 6)
-    private String platform;
-    @ExcelProperty(value = "产品型号", index = 7)
-    private String productModel;
-    @ExcelProperty(value = "数量", index = 8)
-    private Integer num;
-    @ExcelProperty(value = "币种", index = 9)
-    private String currency;
-    @ExcelProperty(value = "价格", index = 10)
-    private BigDecimal price;
-    @ExcelProperty(value = "金额", index = 11)
-    private BigDecimal amount;
-    @ExcelProperty(value = "换货日期", index = 12, format = "yyyy-MM-dd")
-    private String replacementDate;
-    @ExcelProperty(value = "备注", index = 13)
+
+    @ExcelProperty(value = "退货产品线", index = 3)
+    private String returnProductLine;
+    @ExcelProperty(value = "退货BU", index = 4)
+    private String returnBu;
+    @ExcelProperty(value = "退货PDT", index = 5)
+    private String returnPdt;
+    @ExcelProperty(value = "退货平台", index = 6)
+    private String returnPlatform;
+    @ExcelProperty(value = "退货产品型号", index = 7)
+    private String returnProductModel;
+    @ExcelProperty(value = "退货数量", index = 8)
+    private Integer returnNum;
+    @ExcelProperty(value = "退货币种", index = 9)
+    private String returnCurrency;
+    @ExcelProperty(value = "退货价格", index = 10)
+    private BigDecimal returnPrice;
+    @ExcelProperty(value = "客户属性", index = 11)
+    private String returnCustomerttribute;
+    @ExcelProperty(value = "代理费率", index = 12)
+    private String returnAgencyRate;
+    @ExcelProperty(value = "退货金额", index = 13)
+    private BigDecimal returnAmount;
+
+
+    @ExcelProperty(value = "换货产品线", index = 14)
+    private String exchangeProductLine;
+    @ExcelProperty(value = "换货BU", index = 15)
+    private String exchangeBu;
+    @ExcelProperty(value = "换货PDT", index = 16)
+    private String exchangePdt;
+    @ExcelProperty(value = "退货平台", index = 17)
+    private String exchangePlatform;
+    @ExcelProperty(value = "换货产品型号", index = 18)
+    private String exchangeProductModel;
+    @ExcelProperty(value = "换货数量", index = 19)
+    private Integer exchangeNum;
+    @ExcelProperty(value = "退货币种", index = 20)
+    private String exchangeCurrency;
+    @ExcelProperty(value = "换货价格", index = 21)
+    private BigDecimal exchangePrice;
+    @ExcelProperty(value = "换货金额", index = 22)
+    private BigDecimal exchangeAmount;
+    @ExcelProperty(value = "换货日期", index = 23, format = "yyyy-MM-dd")
+    private String exchangeDate;
+
+    @ExcelProperty(value = "备注", index = 24)
     private String remark;
 
     @SuppressWarnings("all")
@@ -46,7 +72,7 @@ public class ReturnsEO extends BaseRowModel {
         this.takeGoodsDate = DateUtil.getFlexDate(takeGoodsDate);
     }
     @SuppressWarnings("all")
-    public void setReplacementDate(String replacementDate) {
-        this.replacementDate = DateUtil.getFlexDate(replacementDate);
+    public void setExchangeDate(String exchangeDate) {
+        this.exchangeDate = DateUtil.getFlexDate(exchangeDate);
     }
 }
