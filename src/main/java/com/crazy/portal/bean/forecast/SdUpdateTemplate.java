8963979a7315e8ac20ad2dcefea5438a698daddd
lee <l_joose@163.com>
2019-10-19 15:24:45 +0800

    Mybatis mapper文件参数增加jdbcType

--- src/main/java/com/crazy/portal/bean/forecast/SdUpdateTemplate.java
+++ src/main/java/com/crazy/portal/bean/forecast/SdUpdateTemplate.java
@@ -35,109 +35,112 @@ public class SdUpdateTemplate extends BaseRowModel {
     @ExcelProperty(index = 7, value = "产品型号")
     private String productModel;
 
-    @ExcelProperty(index = 8, value = "首代销售预测总数量（K）")
+    @ExcelProperty(index = 8, value = "虚拟料号")
+    private String vmNumber;
+
+    @ExcelProperty(index = 9, value = "首代销售预测总数量（K）")
     private String totalSdSaleForecastNum;
 
-    @ExcelProperty(index = 9, value = "本次销售预测总数量（K）")
+    @ExcelProperty(index = 10, value = "本次销售预测总数量（K）")
     private String totalForecastNum;
 
-    @ExcelProperty(index = 10, value = "首代Buffer总数量（K）")
+    @ExcelProperty(index = 11, value = "首代Buffer总数量（K）")
     private String totalSdBufferForecastNum;
 
-    @ExcelProperty(index = 11, value = "客户专货库存截止日期")
+    @ExcelProperty(index = 12, value = "客户专货库存截止日期")
     private String closeDate;
 
-    @ExcelProperty(index = 12, value = "客户未完成的所有专货库存")
+    @ExcelProperty(index = 13, value = "客户未完成的所有专货库存")
     private String delayStock;
 
-    @ExcelProperty(index = 13, value = "月份1")
+    @ExcelProperty(index = 14, value = "月份1")
     private String forecastMonthOne;
 
-    @ExcelProperty(index = 14, value = "上次填写1")
+    @ExcelProperty(index = 15, value = "上次填写1")
     private String lastWriteOne;
 
-    @ExcelProperty(index = 15, value = "本次填写1")
+    @ExcelProperty(index = 16, value = "本次填写1")
     private String currentWriteOne;
 
-    @ExcelProperty(index = 16, value = "GAP1")
+    @ExcelProperty(index = 17, value = "GAP1")
     private String gapOne;
 
-    @ExcelProperty(index = 17, value = "首代Buffer1")
+    @ExcelProperty(index = 18, value = "首代Buffer1")
     private String sdBufferOne;
 
-    @ExcelProperty(index = 18, value = "月份2")
+    @ExcelProperty(index = 19, value = "月份2")
     private String forecastMonthTwo;
 
-    @ExcelProperty(index = 19, value = "上次填写2")
+    @ExcelProperty(index = 20, value = "上次填写2")
     private String lastWriteTwo;
 
-    @ExcelProperty(index = 20, value = "本次填写2")
+    @ExcelProperty(index = 21, value = "本次填写2")
     private String currentWriteTwo;
 
-    @ExcelProperty(index = 21, value = "GAP2")
+    @ExcelProperty(index = 22, value = "GAP2")
     private String gapTwo;
 
-    @ExcelProperty(index = 22, value = "首代Buffer2")
+    @ExcelProperty(index = 23, value = "首代Buffer2")
     private String sdBufferTwo;
 
-    @ExcelProperty(index = 23, value = "月份3")
+    @ExcelProperty(index = 24, value = "月份3")
     private String forecastMonthThree;
 
-    @ExcelProperty(index = 24, value = "上次填写3")
+    @ExcelProperty(index = 25, value = "上次填写3")
     private String lastWriteThree;
 
-    @ExcelProperty(index = 25, value = "本次填写3")
+    @ExcelProperty(index = 26, value = "本次填写3")
     private String currentWriteThree;
 
-    @ExcelProperty(index = 26, value = "GAP3")
+    @ExcelProperty(index = 27, value = "GAP3")
     private String gapThree;
 
-    @ExcelProperty(index = 27, value = "首代Buffer3")
+    @ExcelProperty(index = 28, value = "首代Buffer3")
     private String sdBufferThree;
 
-    @ExcelProperty(index = 28, value = "月份4")
+    @ExcelProperty(index = 29, value = "月份4")
     private String forecastMonthFour;
 
-    @ExcelProperty(index = 29, value = "上次填写4")
+    @ExcelProperty(index = 30, value = "上次填写4")
     private String lastWriteFour;
 
-    @ExcelProperty(index = 30, value = "本次填写4")
+    @ExcelProperty(index = 31, value = "本次填写4")
     private String currentWriteFour;
 
-    @ExcelProperty(index = 31, value = "GAP4")
+    @ExcelProperty(index = 32, value = "GAP4")
     private String gapFour;
 
-    @ExcelProperty(index = 32, value = "首代Buffer4")
+    @ExcelProperty(index = 33, value = "首代Buffer4")
     private String sdBufferFour;
 
-    @ExcelProperty(index = 33, value = "月份5")
+    @ExcelProperty(index = 34, value = "月份5")
     private String forecastMonthFive;
 
-    @ExcelProperty(index = 34, value = "上次填写5")
+    @ExcelProperty(index = 35, value = "上次填写5")
     private String lastWriteFive;
 
-    @ExcelProperty(index = 35, value = "本次填写5")
+    @ExcelProperty(index = 36, value = "本次填写5")
     private String currentWriteFive;
 
-    @ExcelProperty(index = 36, value = "GAP5")
+    @ExcelProperty(index = 37, value = "GAP5")
     private String gapFive;
 
-    @ExcelProperty(index = 37, value = "首代Buffer5")
+    @ExcelProperty(index = 38, value = "首代Buffer5")
     private String sdBufferFive;
 
-    @ExcelProperty(index = 38, value = "月份6")
+    @ExcelProperty(index = 39, value = "月份6")
     private String forecastMonthSix;
 
-    @ExcelProperty(index = 39, value = "上次填写6")
+    @ExcelProperty(index = 40, value = "上次填写6")
     private String lastWriteSix;
 
-    @ExcelProperty(index = 40, value = "本次填写6")
+    @ExcelProperty(index = 41, value = "本次填写6")
     private String currentWriteSix;
 
-    @ExcelProperty(index = 41, value = "GAP6")
+    @ExcelProperty(index = 42, value = "GAP6")
     private String gapSix;
 
-    @ExcelProperty(index = 42, value = "首代Buffer6")
+    @ExcelProperty(index = 43, value = "首代Buffer6")
     private String sdBufferSix;
 
 }
--- src/main/java/com/crazy/portal/util/BusinessUtil.java
+++ src/main/java/com/crazy/portal/util/BusinessUtil.java
@@ -5,6 +5,7 @@ import com.crazy.portal.config.exception.ErrorInfo;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Created by lee on 2019/7/27.
@@ -51,6 +52,11 @@ public class BusinessUtil {
         if(list != null && !list.isEmpty()){
             throw new BusinessException(errorInfo);
         }
+    }
+
+    public static boolean isDateTime(String datetime){
+        Pattern p = Pattern.compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1][0-9])|([2][0-4]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
+        return p.matcher(datetime).matches();
     }
 
 }
