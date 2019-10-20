d1113290f614c930ac8406355a4bad2bce995f9f
lee <l_joose@163.com>
2019-10-20 18:56:47 +0800

    交互管理 - 代码重构

--- src/main/java/com/crazy/portal/util/ErrorCodes.java
+++ src/main/java/com/crazy/portal/util/ErrorCodes.java
@@ -158,7 +158,8 @@ public class ErrorCodes {
         HANDOVER_UPDATE_STATUS_ERROR(40016, "数据未审批，请联系上级进行操作", ""),
         CRM_AMOUNT_ERROR(40017, "请输入有效的CR金额", ""),
         HANDOVER_DATA_EMAIL_ERROR(40018, "获取客户邮箱失败，请联系管理员", ""),
-        HANDOVER_SEND_EMAIL_ERROR(40018, "邮件发送失败，请联系管理员", ""),
+        HANDOVER_SEND_EMAIL_ERROR(40019, "邮件发送失败，请联系管理员", ""),
+        HANDOVER__FIL_DATA_IS_NULL(40020, "数据不能为空，请重新输入", ""),
 
         //客户模块
         CUSTOMER_IS_EMPYT(50000, "没有对应的客户信息", ""),
