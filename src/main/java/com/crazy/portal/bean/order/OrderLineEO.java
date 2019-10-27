4d84ca8e6816f5eccdb6b7eaaefad189e741274c
biao.chen <biao.chen@baozun.com>
2019-10-27 16:44:58 +0800

    目录价格调整

--- src/main/java/com/crazy/portal/bean/order/OrderLineEO.java
+++ src/main/java/com/crazy/portal/bean/order/OrderLineEO.java
@@ -1,5 +1,6 @@
 package com.crazy.portal.bean.order;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,26 +13,29 @@ public class OrderLineEO {
     /**
      * 物料号
      */
+    @ExcelProperty(value = "物料号", index = 0)
     private String productId;
-
+    /**
+     * 客户简称
+     */
+    @ExcelProperty(value = "客户简称", index = 1)
+    private String custAbbreviation;
     /**
      * 平台
      */
+    @ExcelProperty(value = "平台", index = 2)
     private String platform;
     /**
      * 数量
      */
+    @ExcelProperty(value = "数量", index = 3)
     private String num;
 
     /**
      * 期望交货月份
      */
+    @ExcelProperty(value = "期望交货月份(yyyy-MM)", index = 4)
     private String expectedDeliveryMonth;
-
-    /**
-     * 客户简称
-     */
-    private String custAbbreviation;
 
     /**
      * 定价日期
