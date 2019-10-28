5b632f0b7e5f44d8aca57e882597ede712185238
biao.chen <biao.chen@baozun.com>
2019-10-29 00:08:40 +0800

    调价试算接口-新增内部客户编码字段

--- src/main/java/com/crazy/portal/service/order/OrderApplyService.java
+++ src/main/java/com/crazy/portal/service/order/OrderApplyService.java
@@ -443,6 +443,14 @@ public class OrderApplyService {
             itItem.setKondm(sysParameter.getZhName());
             //根据物料号获取平台
             itItem.setPlatform(productInfoDO.getPlatform());
+            String custAbbreviation = orderLineEO.getCustAbbreviation();
+            if(StringUtil.isNotEmpty(custAbbreviation)){
+                CustomerInfo customerInfo = customerInfoMapper.selectInCustomerByAbb(custAbbreviation);
+                if(customerInfo != null){
+                    //如果客户不为空传递内部编码,默认传空字符
+                    itItem.setCustomercode(customerInfo.getInCode());
+                }
+            }
             items.add(itItem);
         }
         return items;
