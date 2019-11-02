72cebcd821b1c245d1026796174111242928f9a3
lee <l_joose@163.com>
2019-11-02 12:01:06 +0800

    交互管理 - 代码重构

--- src/main/java/com/crazy/portal/util/ErrorCodes.java
+++ src/main/java/com/crazy/portal/util/ErrorCodes.java
@@ -161,6 +161,7 @@ public class ErrorCodes {
         HANDOVER_SEND_EMAIL_ERROR(40019, "邮件发送失败，请联系管理员", ""),
         HANDOVER_FILE_DATA_IS_NULL(40020, "数据不能为空，请重新输入", ""),
         HANDOVER_BI_ID_NOT_EMPTY(40021, "提交失败，BI未正确返回ID", ""),
+        HANDOVER_USER_INFO_EMPTY(40022, "用户信息获取失败，请联系管理员", ""),
 
         //客户模块
         CUSTOMER_IS_EMPYT(50000, "没有对应的客户信息", ""),
