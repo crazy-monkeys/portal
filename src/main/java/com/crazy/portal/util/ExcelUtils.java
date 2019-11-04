e09aaef85a9b72dc8eba7e3f74935d5eebb6d46d
junguohe <wy.950202>
2019-11-04 18:56:48 +0800

    modify ExcelUtil

--- src/main/java/com/crazy/portal/util/ExcelUtils.java
+++ src/main/java/com/crazy/portal/util/ExcelUtils.java
@@ -260,24 +260,8 @@ public class ExcelUtils {
         reader.read(readSheet1);
         List<T> data = excelListener.getData();
         reader.finish();
-        BusinessUtil.assertTrue((data != null && data.size() > 0), ErrorCodes.BusinessEnum.BUSINESS_IDR_FILE_IS_REQUIRED);
+        BusinessUtil.assertTrue((data != null && data.size() > 0), ErrorCodes.BusinessEnum.EXCEL_FILE_IS_NULL);
         return data;
-    }
-
-//    private static<T> List<T> readExcel(InputStream in, Class clazz, String fileName, int sheetNo, int headLineNum) throws IOException {
-//        checkExcelType(fileName);
-//        ExcelListener excelListener = new ExcelListener();
-//        ExcelReader reader = getReader(in, excelListener);
-//        Sheet sheet = new Sheet(sheetNo, headLineNum, clazz);
-//        reader.read(sheet);
-//        List<T> data = excelListener.getData();
-//        BusinessUtil.assertTrue((data != null && data.size() > 0), ErrorCodes.BusinessEnum.EXCEL_FILE_IS_NULL);
-//        return data;
-//    }
-
-    private static ExcelReader getReader(InputStream in, ExcelListener excelListener) throws IOException {
-        InputStream inputStream = new BufferedInputStream(in);
-        return new ExcelReader(inputStream, null, excelListener, false);
     }
 
     /**
